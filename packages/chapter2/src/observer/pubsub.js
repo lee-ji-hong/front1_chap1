cbd73d54982b00b4a93a6cbdfed920da237da1c8
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2024-04-08 02:47:52 +0900

    Feat: "답지 확인.."

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,27 @@
-export const 구독 = fn => {
-  fn();
-}
+let currentObserver = null;
 
-export const 발행기관 = obj => {
+export const 구독 = (fn) => {
+  currentObserver = fn;
+  fn();
+  currentObserver = null;
+};
+
+export const 발행기관 = (obj) => {
+  Object.keys(obj).forEach((key) => {
+    let _value = obj[key];
+    const observers = new Set();
+
+    Object.defineProperty(obj, key, {
+      get() {
+        if (currentObserver) observers.add(currentObserver);
+        return _value;
+      },
+
+      set(value) {
+        _value = value;
+        observers.forEach((fn) => fn());
+      },
+    });
+  });
   return obj;
-}
+};
